756c5c0120686ae80e60441d934b4178a227fb81
Robson-Ali <aliye.r@yahoo.com>
2025-11-09 06:11:24 -0800

    duplicate

---
+++ alx-react-app-props/alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+  return (
+    <main>
+      <div style={{ backgroundColor: '#f0f0f0', padding: '20px', borderRadius: '8px', margin: '10px' }}>
+  {<p>I love to visit New York, Paris, and Tokyo.</p>}
+</div>
+    </main>
+  );
+}
+export default MainContent;
