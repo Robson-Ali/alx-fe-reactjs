4274f2c38c7432918098d56285d816dcd7de63a6
Robson-Ali <99282807+Robson-Ali@users.noreply.github.com>
2025-11-09 16:09:58 +0300

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -38,6 +39,17 @@ function App() {
 }
 
 export default App
+
+function App() {
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
 
 function App() {
   return (
