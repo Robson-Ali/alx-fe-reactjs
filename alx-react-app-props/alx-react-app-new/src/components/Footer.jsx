68e8182974f19b1c11f5ab5bcc50a0d33641a7ae
Robson-Ali <aliye.r@yahoo.com>
2025-11-09 06:16:43 -0800

    update

--- alx-react-app-props/alx-react-app-new/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#222', color: '#ddd', padding: '15px', textAlign: 'center' }}>
-  <p>© 2025 My React App</p>
-</footer>
-  );
-}
-export default Footer;
