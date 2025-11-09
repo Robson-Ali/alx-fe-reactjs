f12c095abc09357f279527c7e79a435d33ec6981
Robson-Ali <99282807+Robson-Ali@users.noreply.github.com>
2025-11-09 09:17:37 +0300

    Update UserProfile.jsx

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 const UserProfile = (props) => {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2 style={{ color: 'blue' }}>{props.name}</h2>
+  <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+  <p>Bio: {props.bio}</p>
     </div>
   );
 };
