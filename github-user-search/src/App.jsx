14df112dc9902d50ca12179dbe5fa74c540f9146
Robson-Ali <99282807+Robson-Ali@users.noreply.github.com>
2025-11-23 08:43:40 +0300

    Integrate Search component into App
    Added a Search component and updated the header.

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -3,6 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>GitHub User Search</h1>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
+
 
 function App() {
   return (
