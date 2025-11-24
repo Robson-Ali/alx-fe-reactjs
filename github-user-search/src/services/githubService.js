62c3591860aa28ef0a16785f26aaa536f8fa86f9
Robson-Ali <99282807+Robson-Ali@users.noreply.github.com>
2025-11-24 23:12:57 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,31 +1,26 @@
 import axios from "axios";
 
-async function fetchUserData(username, location = "", minRepos = "", page = 1) {
-  let query = "";
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+export const fetchAdvancedUsers = async (username, location, minRepos) => {
+  const query = [
+    username ? `${username} in:login` : "",
+    location ? `location:${location}` : "",
+    minRepos ? `repos:>=${minRepos}` : "",
+  ].join(" ");
 
-  if (username) query += `${username} `;
-  if (location) query += `location:${location} `;
-  if (minRepos) query += `repos:>${minRepos} `;
-
-  const response = await axios.get(
-    `https://api.github.com/search/users?q=${encodeURIComponent(
-      query.trim()
-    )}&page=${page}&per_page=10`
-  );
-
-  const users = response.data.items;
-
-  const detailedUsers = await Promise.all(
-    users.map(async (user) => {
-      const detail = await axios.get(`https://api.github.com/users/${user.login}`);
-      return { ...user, ...detail.data };
-    })
-  );
-
-  return {
-    items: detailedUsers,
-    total_count: response.data.total_count,
-  };
-}
-
-export default fetchUserData;
+  try {
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${query}`
+    );
+    return response.data.items;
+  } catch (error) {
+    throw error;
+  }
+};
