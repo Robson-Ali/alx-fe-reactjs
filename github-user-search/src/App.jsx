4f6c0d708f9ac0e31e82d7da34db0c38447b0cfe
Robson-Ali <99282807+Robson-Ali@users.noreply.github.com>
2025-11-24 23:14:08 +0300

    Remove header and paragraph, add Search component

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 import Search from "./components/Search";
 
 function App() {
   return (
     <div>
-      <h1>GitHub User Search</h1>
-      <p>App setup successful ✔</p>
+      <Search />
     </div>
   );
 }
